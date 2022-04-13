3a5d389ba8524295f9e01aa7f2f54ca46a8ef30a
Ignasiussigit <martixdna@gmail.com>
2022-04-13 23:58:54 +0700

    submit image

---
+++ script.js
@@ -0,0 +1,30 @@
+let fileInput = document.getElementById("file-input");
+let imageContainer = document.getElementById("images");
+let numOfFiles = document.getElementById("num-of-files");
+
+function preview() {
+    imageContainer.innerHTML = "";
+    numOfFiles.textContent = `${fileInput.files.length} 
+    Files Dipilih`;
+
+    for (i of fileInput.files) {
+        let reader = new FileReader();
+        let figure = document.createElement("figure");
+        let figCap = document.createElement("figcaption")
+            ;
+        figCap.innerText = i.name;
+        figure.appendChild(figCap);
+        reader.onload = () => {
+            let img = document.createElement("img");
+            img.setAttribute("src", reader.result);
+            figure.insertBefore(img, figCap);
+        }
+        imageContainer.appendChild(figure);
+        reader.readAsDataURL(i);
+    }
+}
+
+var nama = prompt("siapa nama anda ?")
+alert("Halo " + nama)
+
+document.getElementById("datang").innerHTML = nama
